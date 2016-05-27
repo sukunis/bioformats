506c777b04a4d5ffc67394012b58de9d2da306b3
kunis <kunis@biologie.Uni-Osnabrueck.DE>
2016-05-27 22:15:55 +0200

    add date formats for parsing acquisition time for tem902 images

--- components/formats-bsd/src/loci/formats/in/BaseTiffReader.java
+++ components/formats-bsd/src/loci/formats/in/BaseTiffReader.java
@@ -80,6 +80,11 @@ public abstract class BaseTiffReader extends MinimalTiffReader {
     "yyyy/MM/dd HH:mm:ss",
     "yyyy-MM-dd'T'HH:mm:ssZ"
   };
+  
+  public static final String[] DATE_FORMATS_EXT={
+	  "dd.MM.yyyy HH:mm:ss:SSS",
+	  "dd.MM.yyyy HH:mm:ss"
+  };
 
   // -- Constructors --
 
@@ -430,6 +435,9 @@ public abstract class BaseTiffReader extends MinimalTiffReader {
     String date = DateTools.formatDate(creationDate, DATE_FORMATS, ".");
     if (creationDate != null && date == null) {
       LOGGER.warn("unknown creation date format: {}", creationDate);
+      date = DateTools.formatDate(creationDate,DATE_FORMATS_EXT);
+      if(date == null)
+    	  LOGGER.warn("unknown uos creation date format: {}", creationDate);
     }
     creationDate = date;
 
